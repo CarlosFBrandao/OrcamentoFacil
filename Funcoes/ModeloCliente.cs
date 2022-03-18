38cb2e9f51ce40db7403a71aa461f1c72c9ba2a1
Carlos Machado <carlos.machado.aluno@faculdadepromove.br>
2022-03-18 10:27:58 -0300

    Implementações gerais

---
+++ Funcoes/ModeloCliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funcoes
+{
+    class ModeloCliente
+    {
+        public int IdCliente { get; set; }
+        public string NomeCliente { get; set; }
+
+    }
+}
